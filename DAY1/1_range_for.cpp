3236b7952bf13d3370176728f509a71fbe2d62b8
ecourse <smkang@codenuri.co.kr>
2024-03-11 14:41:40 +0900

    Add files via upload

--- DAY1/1_range_for.cpp
+++ DAY1/1_range_for.cpp
@@ -10,7 +10,18 @@ int main()
 	for (int i = 0; i < 10; i++)
 		std::cout << x[i] << std::endl;
 
-	// C++11 의 새로운 for 문
-	for ( int n : x )
+	// C++11 의 새로운 for 문 - range for 라고 합니다.
+	// PYTHON : for n in x
+	// C#     : foreach(int n in x)
+
+//	for ( int n : x )
+	for ( auto n : x )	
 		std::cout << n << std::endl;
+
+	// x 위치는 "배열 뿐 아니라 std::vector, std::list" 도 사용가능
+
+	// 전체가 아닌 일부만 ?
+	// 또는 2칸씩 할수 없나요 ?
+	// => C++20 부터는 가능한데, 그 전에는 안됩니다.
+	// => legacy-for 사용하세요.
 }
