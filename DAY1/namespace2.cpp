50940c130d59db68b6a8b73010c4e072c6332e5f
ecourse <smkang@codenuri.co.kr>
2024-03-11 09:44:50 +0900

    Add files via upload

---
+++ DAY1/namespace2.cpp
@@ -0,0 +1,42 @@
+﻿// namespace 에 있는 함수는 접근하는 방법
+#include <stdio.h>
+
+namespace Audio 
+{
+	void init() { printf("Audio init\n");	}
+}
+namespace Video
+{
+	void init()	{ printf("Video init\n");	}
+}
+
+// global namespace : 어떠한 이름공간도 포함되지 않은 공간
+void init()	{ printf("global init\n");	}
+
+// using declaration 과 using directive 는 
+// 함수 안 또는 밖에 있을수 있습니다.
+using Video::init;
+
+int main()
+{
+	// #1. 완전한 이름을 사용한 접근(qualified name)
+	Video::init();
+
+	// #2. using declaration(선언) 사용
+//	using Video::init;	// init 은 Video 이름 없이 사용 가능
+	init();				// Video::init
+	::init();			// global init
+						// :: => scope resolution operator
+
+	// #3. using directive(지시어) 사용
+	using namespace Video; // Video 의 모든 요소를 Video 이름 없이 사용
+	init();
+}
+
+
+
+
+
+
+
+
