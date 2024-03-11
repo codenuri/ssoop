f33abb24591971d8d90144c037bfdd4a2e094a3c
ecourse <smkang@codenuri.co.kr>
2024-03-11 09:34:57 +0900

    Add files via upload

---
+++ DAY1/namespace1.cpp
@@ -0,0 +1,18 @@
+﻿// 교재 6page
+// 확장자 : .cpp    .cxx   .cc
+#include <stdio.h>
+
+namespace Audio 
+{
+	void init() { printf("Audio init\n");	}
+}
+namespace Video
+{
+	// Video 관련 모든 함수, 구조체등을 이 안에서 만들면 됩니다.
+	void init()	{ printf("Video init\n");	}
+}
+
+int main()
+{
+	Video::init();
+}
