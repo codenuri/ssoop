aec681a4072b59f33af76eaa71735795503345d5
ecourse <smkang@codenuri.co.kr>
2024-03-11 10:06:18 +0900

    Add files via upload

---
+++ DAY1/io1.cpp
@@ -0,0 +1,22 @@
+﻿#include <cstdio>
+#include <iostream> 
+
+// 13 page
+int main()
+{
+	int n = 10;
+
+	// C style 입출력
+	printf("n = %d\n", n); // C style
+	scanf_s("%d", &n);	   // 입력
+
+	// C++ style
+	// 1. %d 등을 사용할 필요 없고
+	// 2. << 을 사용해서 계속 출력 가능
+	// 3. std::endl 는 개행(\n) 의미.
+	// printf    : 함수
+	// std::cout : 객체
+	std::cout << "n = " << n << std::endl;
+
+	std::cin >> n; // 입력, &n 가 아닌 n 입니다.
+}
