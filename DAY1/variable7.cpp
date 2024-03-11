6e072db87c2317dacfda78cf9d5ae7b5217575e5
ecourse <smkang@codenuri.co.kr>
2024-03-11 11:15:50 +0900

    Add files via upload

---
+++ DAY1/variable7.cpp
@@ -0,0 +1,17 @@
+﻿// using alias
+// 30 page
+
+//typedef int DWORD;
+//typedef void(*PF)();	// 함수 포인터 타입 만들기
+
+// C++11 부터는 typedef 대신 using 사용하면 됩니다.
+// 아래 2줄이 위 코드와 완전히 동일합니다.
+using DWORD = int;
+using PF = void(*)();
+
+int main()
+{
+	DWORD n; // int n
+	PF    f; 
+}
+
