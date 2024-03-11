5cd3db1bba0d1b79dada5186618084dd80470d62
ecourse <smkang@codenuri.co.kr>
2024-03-11 11:06:23 +0900

    Add files via upload

---
+++ DAY1/variable5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+struct Point
+{
+	int x, y;
+};
+void foo(int n)    {} 
+void goo(Point pt) {}
+
+int main()
+{
+	Point p = { 1,2 };
+
+	foo(3); // 인자의 전달은 "복사 초기화" 과정입니다.
+			// int n = 3;
+	foo({3});// int n = {3};
+
+	goo(p);	
+	goo({1,2}); // Point pt = {1,2} 의 의미.	
+}
+
+
+
+
+
