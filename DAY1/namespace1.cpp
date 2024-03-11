9a466bbf52d543b90580caa83095c3ef379e3c80
ecourse <smkang@codenuri.co.kr>
2024-03-11 09:36:57 +0900

    Add files via upload

--- DAY1/namespace1.cpp
+++ DAY1/namespace1.cpp
@@ -2,6 +2,10 @@
 // 확장자 : .cpp    .cxx   .cc
 #include <stdio.h>
 
+// namespace : 프로그램의 구성 요소를 논리적으로 분리할때 사용
+//             이름 충돌등을 막을수 있다.
+// C언어를 제외한 대부분의 객체지향 언어가 지원.
+
 namespace Audio 
 {
 	void init() { printf("Audio init\n");	}
