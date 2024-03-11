6cd070c6e6afae0f5dc98df770c3872e115a39d8
ecourse <smkang@codenuri.co.kr>
2024-03-11 14:13:45 +0900

    Add files via upload

--- DAY1/함수템플릿4.cpp
+++ DAY1/함수템플릿4.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
+// decltype 키워드가 아래 반환 타입 같은 것을 만들기 위해 도입된 키워드 입니다.
 
 template<typename T1, typename T2>
-? Add(T1 a, T2 b)
+//decltype(a + b) Add(T1 a, T2 b) // error. a, b 를 선언전에 사용하는 코드.
+
+auto Add(T1 a, T2 b) -> decltype(a + b)
 {
 	return a + b;
 }
 int main()
 {
-
 	std::cout << Add(3, 4.1) << std::endl;
+
+//	a = 20; // error. 변수를 선언전에 사용한것
+//	int a = 0;
+//	a = 20;	// ok
 }
