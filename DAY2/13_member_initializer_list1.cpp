b7fc127e16f4f7847ef9f3068112c5be744ee678
ecourse <smkang@codenuri.co.kr>
2024-03-12 14:39:27 +0900

    Add files via upload

---
+++ DAY2/13_member_initializer_list1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	// 생성자에서 멤버 데이타에 값을 넣는 방법
+	
+	// #1. {} 안에서 대입 - 좋은 코드 아닙니다.
+	/*
+	Point(int a, int b) 		
+	{
+		x = a; 
+		y = b;
+	}
+	*/
+
+	// #2. member initializer list(멤버 초기화 리스트) 사용. - good
+	Point(int a, int b) : x(a), y(b) 
+	{
+	}
+
+};
+
+
+
+int main()
+{
+	Point pt(0, 0);
+}
+
+
+
+
