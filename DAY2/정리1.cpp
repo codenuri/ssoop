28624955e4a67011febf8728f05758a811a1e65f
ecourse <smkang@codenuri.co.kr>
2024-03-12 16:12:01 +0900

    Add files via upload

--- DAY2/정리1.cpp
+++ DAY2/정리1.cpp
@@ -6,7 +6,26 @@
 // => auto, constexpr, template, reference, new, range-for 등의 문법
 // => 객체지향언어의 특징은 아니지만 C++ 언어가 제공하는 문법들.. 
 
+
 // 2. 객체지향 언어 개념, 문법
+//    한개의 타입(클래스) 를 만들때 사용하는 문법들
+
+//    => 접근 지정자 : private, public
+//    => 생성자, 소멸자, 
+//       생성자를 한개도 만들지 않으면 컴파일러가 제공
+
+// 	  => 멤버 초기화 리스트 문법
+//       Point(int a, int b) : x{a}, y{b} {}
+
+// 	  => 멤버 함수의 호출 원리 - this
+
+//	  => const member function
+
+//	  => static member data/function
+
+//    => copy constructor
+
+
 
 // 3. 객체지향 디자인
 
