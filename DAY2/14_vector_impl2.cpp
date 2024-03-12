1fb0314bc283fd2fbed6df50521c4518562b4450
ecourse <smkang@codenuri.co.kr>
2024-03-12 12:00:14 +0900

    Add files via upload

--- DAY2/14_vector_impl2.cpp
+++ DAY2/14_vector_impl2.cpp
@@ -10,11 +10,18 @@ private:
 public:
 	vector(int size, int value = 0 )
 	{
+		sz = size;
 		ptr = new int[size];
 
 		for( int i = 0; i < size; i++)
 			ptr[i] = value;
 	}
+
+	int size() { return sz; }
+
+	// 생성자에서 자원(메모리)등을 할당했다면
+	// 소멸자를 만들어서 제거하는 것이 관례입니다.
+	~vector() { delete[] ptr; }
 };
 
 int main()
