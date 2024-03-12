bf5b2a3d3050df848b03c803cdd51ce374a1adb9
ecourse <smkang@codenuri.co.kr>
2024-03-12 15:28:24 +0900

    Add files via upload

--- DAY2/13_member_initializer_list6-1.cpp
+++ DAY2/13_member_initializer_list6-1.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 int g = 10;
-
 class Point
 {
-	int x = 0;
-	int y = ++g;
-public:
-	Point() {}
-	Point(int a) : y{a} {}
+	// 권장 : 멤버데이타(필드)를 직접 초기화 할때는
+	int x = 0;		// 이런 코드는 좋습니다.
+	int y = ++g;	// 이런 코드는 절대 사용하지 마세요
+public:	
+	Point() {}				// : x{0}, y{++g} {}
+	Point(int a) : y{a} {}	// : x{0}, y{a}   {}
 };
 int main()
 {
@@ -16,4 +16,5 @@ int main()
 	Point p2(3);
 
 	std::cout << g << std::endl; // 실행하지 말고 결과 예측해 보세요
+								// => 11
 }
