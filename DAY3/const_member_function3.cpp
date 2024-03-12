ae030a64166c20f329c422722074242361f6027e
ecourse <smkang@codenuri.co.kr>
2024-03-13 08:52:54 +0900

    Add files via upload

---
+++ DAY3/const_member_function3.cpp
@@ -0,0 +1,14 @@
+class Car
+{
+	int speed;
+public:
+	void set_speed(int s) { speed = s;}
+	int get_speed()	      { return speed;}
+};
+
+int main()
+{
+	Car c;
+	c.set_speed(100);
+	int n = c.get_speed();
+}
