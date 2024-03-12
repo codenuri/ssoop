275d4b560757e05d52c2c32471ab207235af1486
ecourse <smkang@codenuri.co.kr>
2024-03-12 13:54:32 +0900

    Add files via upload

--- DAY2/14_std_vector2.cpp
+++ DAY2/14_std_vector2.cpp
@@ -10,12 +10,15 @@ int main()
 	while( 1 )
 	{
 		std::cin >> n;
-
 		if ( n == -1 ) break;
-
 		v.push_back(n);
 	}
 	
 	std::cout << "할당된 버퍼 크기 : " << v.capacity() << std::endl;
 	std::cout << "입력된 정수 갯수 : " << v.size()     << std::endl;
 }
+
+// C++ 에서 알아야 하는 것
+// 1. vector 같은 것을 만들때 사용하는 문법 - class 관련 문법
+// 2. 이미 만들어진 표준라이브러리 vector 의 사용법을 배우는 것
+//    표준라이브러리 STL ( Standard Template Library )
