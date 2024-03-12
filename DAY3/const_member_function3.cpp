1237c265671ad3157d9a311d163fd334d9e90b77
ecourse <smkang@codenuri.co.kr>
2024-03-13 08:55:41 +0900

    Add files via upload

--- DAY3/const_member_function3.cpp
+++ DAY3/const_member_function3.cpp
@@ -1,10 +1,16 @@
 class Car
 {
-	int speed;
+	int speed = 0;
+	static int cnt;
 public:
 	void set_speed(int s) { speed = s;}
-	int get_speed()	      { return speed;}
+//	int get_speed()	      { return speed;}  // 틀린 코드
+	int get_speed()	const { return speed;}  // 맞는 코드.
+
+	static int get_count() { return cnt; } // ??
 };
+int Car::cnt = 0;
+
 
 int main()
 {
