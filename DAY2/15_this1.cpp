d28b4c7d03771e906aae4f3b8d66b550b960d0ef
ecourse <smkang@codenuri.co.kr>
2024-03-12 15:49:29 +0900

    Add files via upload

--- DAY2/15_this1.cpp
+++ DAY2/15_this1.cpp
@@ -10,6 +10,7 @@ public:
 		y = b;				//		this->y = b;   로 변경됩니다.
 
 		// 멤버 함수 안에서 this를 직접 사용해도 됩니다.
+		// this : 현재 멤버함수를 호출할때 사용한 객체의 주소
 		std::cout << this << std::endl;
 	}
 };
@@ -21,7 +22,6 @@ int main()
 
 	std::cout << "p1 : " << &p1 << std::endl;
 	std::cout << "p2 : " << &p2 << std::endl;
-
 
 	p1.set(10, 20); // Point::set(&p1, 10, 20) 의 의미의 코드로 변경됩니다.
 	p2.set(10, 20); // Point::set(&p2, 10, 20)
