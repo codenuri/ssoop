911cf55dbaea7be2c4945989a6fdeb99662f0475
ecourse <smkang@codenuri.co.kr>
2024-03-13 10:42:38 +0900

    Add files via upload

---
+++ DAY3/정리1.cpp
@@ -0,0 +1,29 @@
+// 정리1.cpp
+
+// 1. 객체지향 문법은 아니지만 C 보다 발전된 C++ 문법들 ( 1일차)
+// => auto, constexpr, template, reference .... 
+
+
+// 2. 한개의 타입을 만들때 사용하는 문법(class 문법) ( 2일차 ~ 3일차 2교시)
+
+// => private, public
+// => 생성자, 소멸자
+// => 멤버 초기화 리스트(member initializer list)
+
+// => static member data, 
+//    static member function
+
+// => const member function  ====> C++ 언어 만의 특징.
+
+// => copy constructor( 복사 생성자 )
+
+
+// 3. 프로그램 전체를 객체지향 기법으로 만들어 가는 기술
+// => 객체지향 디자인
+// => 상속 문법 부터 시작 ~~~~
+
+
+// 4. 기타 문법(예외, 연산자 재정의)
+
+
+// 5. STL(C++ 표준라이브러리) 사용법
