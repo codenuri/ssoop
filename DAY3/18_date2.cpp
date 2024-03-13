ed6ab857846e6db55436affe70fc1e0a4e64594b
ecourse <smkang@codenuri.co.kr>
2024-03-13 09:46:36 +0900

    Add files via upload

--- DAY3/18_date2.cpp
+++ DAY3/18_date2.cpp
@@ -10,18 +10,24 @@ private:
 	int month;
 	int day;
 public:
+	Date(int year, int month, int day) : year{year}, month{month}, day{day} {}
 
+	void print() const
+	{
+		std::cout << year << "-" << month << "-" << day << std::endl;
+	}
+	void set_year(int y)  { year = y;}
+	int  get_year() const { return year;}
 };
 
 int main()
 {
 	Date d1{2024, 3, 12};	// #1. 생성자 만드세요
-
 	d1.print();				// #2. "2024-3-12" 로 출력되게 하세요
-
 	d1.set_year(2022);		// #3. setter/getter
 	int y = d1.get_year();
 
+	std::cout << y << std::endl;
 }
 
 
