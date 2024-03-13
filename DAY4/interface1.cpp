a84dc9c6b44a6e6168eb4d74413605dd2d33d2aa
ecourse <smkang@codenuri.co.kr>
2024-03-14 08:56:59 +0900

    Add files via upload

---
+++ DAY4/interface1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Camera
+{
+public:
+	void take() { std::cout << "take picture" << std::endl; }
+};
+
+class HDCamera
+{
+public:
+	void take() { std::cout << "take HD picture" << std::endl; }
+};
+
+
+class Person
+{
+public:
+	void use_camera(Camera* c)   { c->take(); }
+	void use_camera(HDCamera* c) { c->take(); }
+};
+
+int main()
+{
+	Person p;
+	Camera c;
+
+	p.use_camera(&c);	
+
+	HDCamera hc;
+	p.use_camera(&hc); // ??
+}
+
