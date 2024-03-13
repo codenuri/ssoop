590ee9b2004cd93b78205fecd6c3aca96bee48b3
ecourse <smkang@codenuri.co.kr>
2024-03-13 15:07:54 +0900

    Add files via upload

--- DAY3/shape2.cpp
+++ DAY3/shape2.cpp
@@ -27,19 +27,24 @@ int main()
 
 		if ( cmd == 1 )
 		{
-			Rect rc;
+//			Rect rc;			// 이렇게 만들면 {} 벗어날때 즉시 파괴 됩니다.
+			Rect* p = new Rect; // 이렇게 해야, 사용자가 제거할때 까지 계속사용가능
+
+			v.push_back(p);
 		}
 		else if ( cmd == 2 )
 		{
+			v.push_back(new Circle);
 		}
 		else if ( cmd == 9 )
 		{
 			for( auto s : v)
 				s->draw();
 		}
-	}
-	
+	}	
 }
+// 위 코드는 어디서 왜??? 에러 일까요 ??
+// 해결책은 뭘까요 ? => 다음 단계에서 해결
 
 
 
