f67eb10e2429bcae53e9615eae213c843cd5a106
ecourse <smkang@codenuri.co.kr>
2024-03-13 09:09:58 +0900

    Add files via upload

--- DAY3/const_member_function4.cpp
+++ DAY3/const_member_function4.cpp
@@ -7,16 +7,19 @@ class Point
 public:
 	Point( int x, int y) : x{x}, y{y} {}  // 초기화 리스트에서는 이름이 동일해도 ok
 
-	const char* to_string()
+	// 객체의 상태를 문자열로 반환하는 함수
+	// => const member function 으로 할까요 ? 말까요 ?
+	char* to_string() const 
 	{
-		char s[32];
+		static char s[32];
 		sprintf(s, "(%d, %d)", x, y);
 		return s;
 	}
 };
 int main()
 {
-	Point pt{1,2};
+//	Point pt{1,2};
+	const 	Point pt{1,2};
 	std::cout << pt.to_string() << std::endl;
 }
 
