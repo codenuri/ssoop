9d913bea9a7737c9133bbbce2569997b00aec2bb
ecourse <smkang@codenuri.co.kr>
2024-03-13 10:21:19 +0900

    Add files via upload

--- DAY3/18_date5.cpp
+++ DAY3/18_date5.cpp
@@ -60,8 +60,6 @@ public:
 };
 int Date::days[12] = { 31, 28, 31, 30, 31, 30,  31, 31, 30, 31,30,31};
 
-
-
 int main()
 {
 	Date d1{2024, 3, 12};	
@@ -70,6 +68,16 @@ int main()
 	// => static 으로 할까요 ? non-static 으로 할까요 ?
 	int ds = Date::how_many_days(5);
 	int ds = Date::how_many_days(2, 2024);
+
+
+	// 나중에 복습하실때 아래 멤버 함수 구현해 보세요
+	// => 회사에 입사한후 1000일이 되면 "여행상품권" 을 받게 된다.!!
+	// => 1000일뒤의 날짜를 알고 싶다.
+	// => static 으로 할까요 ? non-static 으로 할까요 ?
+	// => "기준이 되는 날짜(객체)" 가 필요 합니다. - non static
+
+	Date d2{2024, 3, 12};
+	Date d3 = d2.after_days(1000);  // 복습할때 구현해 보세요.
 }
 
 
