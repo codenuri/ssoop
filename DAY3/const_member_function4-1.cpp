ae7fc551f2336538b1b1126cec0ceef8b5c378a7
ecourse <smkang@codenuri.co.kr>
2024-03-13 09:18:18 +0900

    Add files via upload

--- DAY3/const_member_function4-1.cpp
+++ DAY3/const_member_function4-1.cpp
@@ -5,8 +5,11 @@ class Point
 	int x;
 	int y;
 
-	char cache[32];
-	bool cache_valid = false;
+	// mutable 멤버 데이타 : 상수 멤버 함수 안에서도 값을 변경할수 있게 해달라.
+	//						멤버함수가 논리적으로는 "상수 멤버함수"가 되어야 하는데
+	//						실제 구현에서, 멤버데이타의 변경이 필요 할때 사용.
+	mutable char cache[32];
+	mutable bool cache_valid = false;
 public:
 	Point( int x, int y) : x{x}, y{y} {}
 
