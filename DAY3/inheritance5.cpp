4258ba0bf72c0d51038cedce3ad8a02ab5e33c41
ecourse <smkang@codenuri.co.kr>
2024-03-13 11:16:03 +0900

    Add files via upload

---
+++ DAY3/inheritance5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+class Person
+{
+	std::string name;
+	int age;
+public:
+	Person( const std::string& name, int age) : name{name}, age{age}{}
+};
+
+// Person 으로 부터 상속받는 Student 만들어 보세요
+// => 학번(int id) 추가하세요
+// => Student 생성자 만드세요
+// => main 에서 Student 객체 생성해 보세요
+
+
+int main()
+{
+//	Person p;	// 이렇게 객체를 만들수 있게 하면 좋을까요 ?
+				// 이름과 나이가 초기화 되지 않은 쓰레기값을 가지는 객체
+}
+
+
+
