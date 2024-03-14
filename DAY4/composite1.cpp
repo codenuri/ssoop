5ba39f09b0ee0823e4465b2d9582b51fe0311e7f
ecourse <smkang@codenuri.co.kr>
2024-03-14 12:09:21 +0900

    Add files via upload

--- DAY4/composite1.cpp
+++ DAY4/composite1.cpp
@@ -1,6 +1,12 @@
 // composite1.cpp
 #include <iostream>
 
+// 아래 코드가 실행되려면
+// => Folder, File 클래스가 필요 합니다.
+// => Menu 예제를 참고해서 만들어 복습할때 만들어 보세요
+// => "COMPOSITE" 패턴이라는 디자인 패턴 입니다.
+// => 구글에서 "COMPOISTE" 검색후, "이미지" 보기
+
 int main()
 {
 	Folder* fo1 = new Folder("root");
@@ -14,7 +20,7 @@ int main()
 	fo1->add(fo3);
 
 	fo2->add(f1);
-	fo3->add(f2);
+	fo1->add(f2);
 
 	std::cout << f1->get_size() << std::endl;
 	std::cout << fo3->get_size() << std::endl;
