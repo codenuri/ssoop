b65506c4f8e1e91f8ba07ccf9db06b72f836d772
ecourse <smkang@codenuri.co.kr>
2024-03-14 14:47:14 +0900

    Add files via upload

--- DAY4/sptr2.cpp
+++ DAY4/sptr2.cpp
@@ -15,6 +15,8 @@ public:
 	Ptr(Car* p = nullptr) : obj{p} {}
 
 	Car* operator->() { return obj; } // <====== 핵심!!!
+
+	~Ptr() { delete obj; }
 };
 
 int main()
@@ -22,8 +24,14 @@ int main()
 //	Car* p = new Car; 
 //	delete p;
 
-	Car p( new Car );
+//	Ptr p( new Car );
+	Ptr p = new Car;
 
-	p->go();
+	// p : Car 가 아닌 Ptr 타입
+	//     포인터가 아닌 객체
+
+	// "Ptr 의 객체" p 를 "Car의 포인터" 처럼 사용
+	p->go(); // ( p.operator->() ) go() 이지만 컴파일러는 
+			 // ( p.operator->() )->go() 처럼 해석해 줍니다.
 
 }
