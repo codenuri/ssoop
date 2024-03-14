81712556b0a18769e53d6d02432e08841636fbfb
ecourse <smkang@codenuri.co.kr>
2024-03-14 13:49:04 +0900

    Add files via upload

--- DAY4/operator_overloading2.cpp
+++ DAY4/operator_overloading2.cpp
@@ -10,11 +10,13 @@ public:
 
 	friend Point operator+(const Point& p1, const Point& p2);
 };
+// + 연산 사용시
+// C++의 약속된 함수     : operator+
+// PYTHON 의 약속된 함수 : __add__ 
 
 Point operator+(const Point& p1, const Point& p2)
 {
 	std::cout << "non-member operator+\n";
-
 	Point tmp{p1.x + p2.x,  p1.y + p2.y};
 
 	return tmp;
@@ -26,5 +28,7 @@ int main()
 	Point p2(2, 2);
 
 	Point p3 = p1 + p2;
+
+	p3.print();
 }
 
