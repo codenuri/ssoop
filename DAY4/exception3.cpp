23ecaeacf61f95ee2157e7d5f7c9c6aead6f2f03
ecourse <smkang@codenuri.co.kr>
2024-03-14 15:39:08 +0900

    Add files via upload

---
+++ DAY4/exception3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// 2. 호출자는 예외 가능성이 있는 함수를 호출할때
+// => try {} 안에서 호출해야 하고
+// => catch 로 던져진 예외를 잡아야 합니다.
+
+void db_remove() { std::cout << "db remove\n"; }
+
+int db_backup()
+{
+	if (1)  
+		throw 1;	
+	return 100;
+}
+
+int main()
+{
+	try 
+	{
+		int ret = db_backup();
+	}
+	catch(int e)
+	{
+		std::cout << "예외 발생\n";
+
+		// 여기서 원인을 파악하고, 오류를 수정합니다.
+		// 오류를 해결할수 없다면 사용자에게 알리고 프로그램을 종료해야 합니다.
+		std::exit(-1);
+
+		// 여기서 종료하지 않으면 프로그램을 계속 실행됩니다.
+	}
+
+	db_remove();
+}
