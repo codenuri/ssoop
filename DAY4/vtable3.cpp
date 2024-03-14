8d2aaf167537b64027c444a4bf228b229b7da67f
ecourse <smkang@codenuri.co.kr>
2024-03-14 10:20:08 +0900

    Add files via upload

--- DAY4/vtable3.cpp
+++ DAY4/vtable3.cpp
@@ -5,11 +5,14 @@ class Point
 	int x;
 	int y;
 public:
+
+	virtual void foo() {}
 };
 
 int main()
 {
 	Point pt;
 
+	// sizeof : pt 객체(변수)가 사용하면 메모리 크기를 구하는 연산자
 	std::cout << sizeof(pt) << std::endl;
 }
