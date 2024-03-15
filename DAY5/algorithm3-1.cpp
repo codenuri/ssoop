6f43ad83b6a9eee2f272a2b121d006b738d0c227
ecourse <smkang@codenuri.co.kr>
2024-03-15 09:03:12 +0900

    Add files via upload

---
+++ DAY5/algorithm3-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = { 1,2,9,4,3,6,7,3,9,10 };
+
+	// #1. v에서 처음 나오는 짝수를 찾아서 출력해 보세요. 검색 실패도 출력해 보세요
+
+
+	// #2. "3번째(9)요소 ~  8번째 요소(3, 포함)" 에서 짝수 검색해 보세요 
+}
