1da78e0ebea3d25743812b1d55afa0e7d8eeb429
ecourse <smkang@codenuri.co.kr>
2024-03-15 12:13:44 +0900

    Add files via upload

--- DAY5/1_cout5.cpp
+++ DAY5/1_cout5.cpp
@@ -14,6 +14,11 @@ int main()
 		if ( std::cin.fail() )
 		{
 			std::cout << "입력실패. 다시 입력하세요\n";
+
+			std::cin.clear();  // 실패/성공을 관리하는 멤버를 reset
+			std::cin.ignore(256, '\n'); // '\n' 를 만날때 까지 
+										// 최대 256자 입력 버퍼 비우기
+
 		}
 		else
 			break;
