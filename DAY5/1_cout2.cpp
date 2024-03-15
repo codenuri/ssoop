a520f8b0f40479f98dfdc1552255670d6927e7a7
ecourse <smkang@codenuri.co.kr>
2024-03-15 11:40:10 +0900

    Add files via upload

--- DAY5/1_cout2.cpp
+++ DAY5/1_cout2.cpp
@@ -6,7 +6,11 @@ namespace std
 	class ostream
 	{
 	public:
-
+		// 아래 2개 외에도 모든 표준 타입에 대해서 << 연산자 제공
+		// => 실제 구현은 printf() 가 아닌 OS 가 제공하는 함수 사용해서 출력
+		void operator<<(int n)    { printf("%d", n);}
+		void operator<<(double d) { printf("%lf", d);}
+		void operator<<(const char* s) { printf("%s", s);}
 	};
 	ostream cout;  // 미리 만들어진 전역 객체
 }
