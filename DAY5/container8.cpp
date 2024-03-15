e0a86447bbb75c9d543d519a9fd54d23993bc256
ecourse <smkang@codenuri.co.kr>
2024-03-15 11:18:34 +0900

    Add files via upload

--- DAY5/container8.cpp
+++ DAY5/container8.cpp
@@ -8,14 +8,14 @@ int main()
 	std::vector<int> v3;
 	v3.reserve(1000);		    // size = 0,    capacity = 1000
 
-	v1.push_back(0);
-	v2.push_back(0);
-	v3.push_back(0);
+	v1.push_back(0);	// 메모리 재할당, 한개 메모리 할당됨.
+	v2.push_back(0);	// 메모리 재할당, 2000개 메모리 할당
+	v3.push_back(0);	// 메모리 재할당 안하고, 기존 1000개 에 1번째 요소에 넣기
 
 	// 실행하지 말고, 아래 3줄 결과 예측하세요
-	std::cout << v1.size() << ", " << v1.capacity() << std::endl;
-	std::cout << v2.size() << ", " << v2.capacity() << std::endl;
-	std::cout << v3.size() << ", " << v3.capacity() << std::endl;
+	std::cout << v1.size() << ", " << v1.capacity() << std::endl; // 1, 1
+	std::cout << v2.size() << ", " << v2.capacity() << std::endl; // 1001, 2000
+	std::cout << v3.size() << ", " << v3.capacity() << std::endl; // 1, 1000
 
 
 }
