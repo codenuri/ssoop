41e7f426e5b669d425716f4de22da7eb1a6339f0
ecourse <smkang@codenuri.co.kr>
2024-03-15 09:14:34 +0900

    Add files via upload

--- DAY5/algorithm4.cpp
+++ DAY5/algorithm4.cpp
@@ -11,5 +11,13 @@ int main()
 
 	auto ret1 = std::find_if( v.begin(), v.end(), is_even );
 
+	// 람다 표현식 : 익명의 함수를 만드는 문법
+	// 			    함수의 이름(주소) 가 전달되는 위치에 함수 구현 자체를 만드는 문법
+
+	// [] : 람다가 시작됨을 알리는 "lambda introducer"
+	
+	auto ret2 = std::find_if( v.begin(), v.end(),  
+					[](int n) { return n % 2 == 0; }     );
+
 }
 
