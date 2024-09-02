be1e4cbdf807320d4810f1958ad751c235e83751
ecourse <smkang@codenuri.co.kr>
2024-09-02 17:02:41 +0900

    Add files via upload

--- DAY1/9_레퍼런스3-1.cpp
+++ DAY1/9_레퍼런스3-1.cpp
@@ -16,3 +16,19 @@ int main()
 	f1(a);
 	f2(a);
 }
+
+// 함수 인자로 받은 것을 수정하려고 합니다.
+// => pointer 또는 reference 로 받으면 됩니다.
+// => 그런데, reference 가 안전 합니다.
+// 
+// void f1(int* p) {} // 가능하지만, 안전성 부족
+// void f2(int& r) {} // ok. 위 보다 안전
+
+
+// 함수 인자로 받은 것을 읽기만 하려고 합니다.
+
+// 1. 인자타입이 primitive(int, double등)인 경우 - call by value
+//    void f1(int n) {}
+
+// 2. 인자 타입이 user define type(구조체, 클래스) - const reference
+//    void f2(const Rect& rc) {}
