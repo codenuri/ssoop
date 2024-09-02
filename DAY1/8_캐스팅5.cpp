2e5104d93e8175e2dae877ed811003d78efc864b
ecourse <smkang@codenuri.co.kr>
2024-09-02 16:00:21 +0900

    Add files via upload

---
+++ DAY1/8_캐스팅5.cpp
@@ -0,0 +1,17 @@
+﻿int main()
+{
+	const int c = 10;
+
+	// c의 주소를 double* 에 담고 싶습니다.
+//	double* p = (double*)&c; // ok..
+
+	// C++ 스타일 캐스팅으로 위 코드 만들어보세요
+
+	double* p = ??   &c; // ok..
+
+}
+
+
+
+
+
