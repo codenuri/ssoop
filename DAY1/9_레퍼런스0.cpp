f57d8c699527fcde88ceab4632a06eec0d57a113
ecourse <smkang@codenuri.co.kr>
2024-09-02 16:23:41 +0900

    Add files via upload

---
+++ DAY1/9_레퍼런스0.cpp
@@ -0,0 +1,22 @@
+﻿// 1_레퍼런스.cpp    57 page
+
+#include <iostream>
+
+int main()
+{
+	int n1 = 10;
+
+	int& r1 = n1;
+	// 타입& r1 = 다른변수;
+
+	r1 = 30; // 결국 n1 = 30
+
+	std::cout << n1  << std::endl; // 30
+	
+	std::cout << &r1 << std::endl;
+	std::cout << &n1 << std::endl; // 위와 동일 주소
+
+}
+
+
+
