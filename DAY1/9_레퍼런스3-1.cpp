aedcf6ccb70232540ea21668ecbb4819d1c22a5e
ecourse <smkang@codenuri.co.kr>
2024-09-02 16:55:09 +0900

    Add files via upload

---
+++ DAY1/9_레퍼런스3-1.cpp
@@ -0,0 +1,14 @@
+// 9_레퍼런스3-1.cpp
+
+// 인자로 받은 것을 읽기만 하려고 합니다.
+// 다음중 좋은 코드는  ?
+
+void f1(int n)        { int a = n; }
+void f2(const int& n) { int a = n; }
+
+int main()
+{
+	int a = 10;
+	f1(a);
+	f2(a);
+}
