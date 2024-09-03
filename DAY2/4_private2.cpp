145b1ae0c67095feb538a62e5aa3b4abb5e77d71
ecourse <smkang@codenuri.co.kr>
2024-09-03 10:13:24 +0900

    Add files via upload

--- DAY2/4_private2.cpp
+++ DAY2/4_private2.cpp
@@ -4,9 +4,17 @@
 
 // struct vs class 
 
-struct Person
+// struct : 접근 지정자를 표기하지 않으면 디폴트가 public
+// class  : 접근 지정자를 표기하지 않으면 디폴트가 private
+
+// => 위 차이점 외에는 "struct" 와 "class" 는 완전히 동일합니다.
+// => 단, Java, C#은 아주 큰 차이가 있습니다.
+// => Rust 언어는 "struct" 만 있습니다. 
+
+//struct Person
+class Person
 {
-private:				
+//private:				
 	std::string name;	
 	int  age;			
 
