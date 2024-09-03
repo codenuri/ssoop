4eb75d8edc857716fb7f68a85f8b5cd1a3ef7d6f
ecourse <smkang@codenuri.co.kr>
2024-09-03 11:16:50 +0900

    Add files via upload

--- DAY2/6_member_initializer_list4-1.cpp
+++ DAY2/6_member_initializer_list4-1.cpp
@@ -2,10 +2,12 @@ class Test
 {
 	const int c;
 public:
-	Test(int k)
+	Test(int k) : c{ k } // ok. const int c = k
 	{
-		c = k; // ?
-	}
+	//	c = k; // const int c; 만들고
+			   // c = k 한것. 
+				// error. 
+	}			
 };
 
 int main()
