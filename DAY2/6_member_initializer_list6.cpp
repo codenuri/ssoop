17a55396b99bf424ee28dd1ed16cc443a3959fae
ecourse <smkang@codenuri.co.kr>
2024-09-03 11:39:35 +0900

    Add files via upload

--- DAY2/6_member_initializer_list6.cpp
+++ DAY2/6_member_initializer_list6.cpp
@@ -14,13 +14,22 @@ class Rect
 	Point leftTop;
 	Point rightBottom;
 public:
-	Rect()
+
+	// 사용자 코드			// 컴파일러가 변경한 코드
+	Rect()					// Rect() : leftTop{}, rightBottom{}
 	{
+		// 이 위치에 기계어 코드로
+		// leftTop, rightBottom 생성자 호출 코드 생성
+		// => C++로 표현할때는 위처럼 "초기화 리스트에 표현"
+
 		std::cout << "Rect()\n";
 	}
 };
 
+
+
 int main()
 {
 	Rect rc; // 실행결과 확인해 보세요
+			 // call Rect::Rect() 
 }
