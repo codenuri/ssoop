c22e210e976d7a663f26be245495d3cdf7e8e56a
ecourse <smkang@codenuri.co.kr>
2024-09-03 17:06:48 +0900

    Add files via upload

--- DAY2/10_const_member_function10.cpp
+++ DAY2/10_const_member_function10.cpp
@@ -4,21 +4,30 @@
 class Point
 {
 	int x, y;
+
+	char cache[32];
+	bool valid = false;
 public:
 	Point(int a, int b) : x{ a }, y{ b } {}
 
 	// 객체의 상태를 문자열로 반환하는 함수
 	// => C#, Java 에서 널리 사용되는 함수
-	const char* to_string()
+	// => 객체의 상태를 읽기만 하므로 "const member function" 이어야 합니다.
+	const char* to_string() const 
 	{
-		static char s[32];
-		sprintf(s, "x = %d, y = %d", x, y);
+		if (valid == false)
+		{
+			sprintf(cache, "x = %d, y = %d", x, y);
+			valid = true;
+		}
 
-		return s;
+		return cache;
 	}
 };
 int main()
 {
 	Point pt(1, 2);
+
+	std::cout << pt.to_string() << std::endl;
 	std::cout << pt.to_string() << std::endl;
 }
