216ce5890c24d3fc4bce4758e1241c6c97465db4
ecourse <smkang@codenuri.co.kr>
2024-09-03 13:25:47 +0900

    Add files via upload

--- DAY2/정리.cpp
+++ DAY2/정리.cpp
@@ -7,6 +7,22 @@
 // => C++11 이후 추가된것들 : auto, decltype, {} 초기화, constexpr
 
 // #2. 객체지향 문법(class 문법)
+// => 프로그램에서 필요한 타입(Rect, Point, Person)을 먼저 만들어라!!
+// => 안전하고, 사용하기 쉽게 만들기 위한 문법을 알아야 한다.
+
+// 1. private, public
+// 2. 생성자 개념, 특징
+//    => 멤버 데이타의 생성자 호출원리
+// 
+// 3. 멤버 초기화 리스트(member initializer list)
+
+// 4. 소멸자 
+
+// 5. this
+// 6. const member function
+// 7. static 
+// 8. 복사 생성자
+
 
 // #3. 객체지향 디자인(상속문법)
 
