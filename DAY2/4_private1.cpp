6665a3230d1cfeb15f0d079029483d4884bb1a86
ecourse <smkang@codenuri.co.kr>
2024-09-03 10:02:30 +0900

    Add files via upload

--- DAY2/4_private1.cpp
+++ DAY2/4_private1.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <string>
 
+// 캡슐화(encapsulation)
+// => 멤버 데이타를 private 놓고,
+// => 잘정의된 public 멤버 함수에 의해서만 변경되게 한다
+// => 외부의 잘못된 사용으로 부터 객체의 상태가 불안해 지는 것을 막는다.
+
+
 struct Person
 {
 private:				// private 영역
@@ -11,7 +17,9 @@ private:				// private 영역
 public:			// public 영역 : 외부에서도 접근 가능
 	void set_age(int a)
 	{
-		age = a; // ok. 멤버 함수에서는 private 멤버 접근 가능
+		// 인자 값이 유효한 경우만 멤버의 상태를 변경한다.
+		if ( a > 0 && a < 150  )
+			age = a; // ok. 멤버 함수에서는 private 멤버 접근 가능
 	}
 };
 int main()
