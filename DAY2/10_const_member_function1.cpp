e4ed6be2862c05d331b76cfb94a364890622d070
ecourse <smkang@codenuri.co.kr>
2024-09-03 15:57:33 +0900

    Add files via upload

--- DAY2/10_const_member_function1.cpp
+++ DAY2/10_const_member_function1.cpp
@@ -25,14 +25,14 @@ public:
 
 	// const member function
 	// 1. 멤버 함수 뒤쪽에 const 를 붙이는 문법. 
-	// 2. 멤버 함수에서 멤버 데이타를 수정할수 없다.
+	// 2. const 멤버 함수에서 멤버 데이타를 수정할수 없다.
 	// 3. 상수객체라도 const member function 은 호출할수 있다.
-
+	// 원리 : this 가 const Point* 로 받는것
 
 //	void print()		// void print(Point* this)
-	void print() const 
+	void print() const  // void print(const Point* this)
 	{	
-		x = 10; // error. 상수 멤버 함수는 멤버 데이타 수정 안됨
+//		x = 10; // error. 상수 멤버 함수는 멤버 데이타 수정 안됨
 
 		std::cout << x << ", " << y << std::endl;
 	}
@@ -46,6 +46,8 @@ int main()
 	p.x = 10;		// error. public 이지만 p가 상수객체이므로
 	p.set(10, 20);	// error.
 	p.print();		// error
+					// 단, print 가 상수 멤버함수라면 가능
 }
 
-// 핵심 #1. 상수 객체는 ? 멤버 함수를 호출할수 없다.
+// 핵심 #1. 상수 객체는 non-const 멤버 함수를 호출할수 없다.
+// 핵심 #2. 상수 객체는 const 멤버 함수를 호출할수 있다.
