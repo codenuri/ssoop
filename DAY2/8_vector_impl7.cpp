d15bbb5e936bde5d0ea8b946e9462607625a67b5
ecourse <smkang@codenuri.co.kr>
2024-09-03 14:53:45 +0900

    Add files via upload

--- DAY2/8_vector_impl7.cpp
+++ DAY2/8_vector_impl7.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include "vector.h"
+//#include "vector.h"
+#include <vector> // 이미 C++ 표준에 vector 있습니다.
 
 int main()
 {
-	vector<double> v(4, 3.1);
+//	vector<double> v(4, 3.1);
+	std::vector<double> v(4, 3.1); // C++ 표준 vector
+									// 위 코드와 유사하지만
+									// 훨씬 효율적이고 복잡하게 설계됨.
 
 	v.at(0) = 5.4;
 	v.resize(8);
--- DAY2/8_vector_impl8.cpp
+++ DAY2/8_vector_impl8.cpp
@@ -16,3 +16,23 @@ int main()
 	std::cout << "입력된 갯수       : " << v.size() << std::endl;
 }
 
+// C++ 표준에서 class 문법으로 많은 좋은 클래스를 이미 제공합니다.
+// => 대부분 template 으로 되어 있습니다
+// => "STL" 이라고 합니다. "Standard Template Library"
+
+// => std::string, std::vector, std::list, std::set(tree)
+//    std::unordered_set(hash), std::thread, std::second
+
+
+// C++ 을 통해서
+
+// 1. std::vector 같은 클래스를 만드는 법을 알아야 합니다.
+// => 클래스 문법
+
+// 2. std::vector 의 사용법을 알아야 합니다.
+// => 라이브러리 활용법.
+
+// 참고 : python 의 list 가 std::vector 와 동일
+
+// s = [1, 1, 1]; // std::vector<int> v(1,3) 과 완전 동일
+// s.append(2);   // v.push_back(2)
