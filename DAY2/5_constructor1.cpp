8205d7141ec398bd4b2c85818c8464a4e7289255
ecourse <smkang@codenuri.co.kr>
2024-09-03 10:22:15 +0900

    Add files via upload

--- DAY2/5_constructor1.cpp
+++ DAY2/5_constructor1.cpp
@@ -10,7 +10,16 @@ private:
 	int  age;
 
 public:
-	void init(std::string n, int a)
+	void init(const std::string& n, int a)
+	{
+		name = n;
+		age = a;
+	}
+	// 생성자
+	// 1. 클래스 이름과 동일한 함수
+	// 2. 반환 타입을 표기하지 않는다.
+	// 3. 객체를 만들면 자동으로 호출된다.
+	Person(const std::string& n, int a)
 	{
 		name = n;
 		age = a;
@@ -26,8 +35,11 @@ int main()
 
 	// #2. 멤버 데이타가 private 에 있는 경우
 	//     멤버 함수로 초기화해야 합니다.
-	Person p;
-	p.init("kim", 28);
+//	Person p;
+//	p.init("kim", 28);
+
+	// #3. 생성자를 사용한 객체 초기화
+	Person p("kim", 28);
 }
 
 
