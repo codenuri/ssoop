980c1de0eb869b23c63b2d751107d0234bf62af3
ecourse <smkang@codenuri.co.kr>
2024-09-04 15:39:24 +0900

    Add files via upload

--- DAY3/6_shape1.cpp
+++ DAY3/6_shape1.cpp
@@ -6,14 +6,23 @@
 
 // 1. 모든 도형을 클래스로 설계 해야 한다.
 
+// 2. 모든 도형의 기반 클래스가 있다면..
+// => 색상등의 공통의 특징을 제공할수 있고
+// => 모든 도형을 한개의 컨테이너에 같이 관리할수 있다.
 
-class Rect
+class Shape
+{
+	int color;
+};
+
+class Rect : public Shape
 {
 	// int x, y, w, h,  생성자등은 생략
 public:
 	void draw() { std::cout << "draw rect\n"; }
 };
-class Circle
+
+class Circle : public Shape
 {
 public:
 	void draw() { std::cout << "draw circle\n"; }
@@ -21,7 +30,8 @@ public:
 
 int main()
 {
-	std::vector<Rect*> v;
+//	std::vector<Rect*> v; // Rect 객체만 보관 가능.
+	std::vector<Shape*> v; // 모든 도형 객체를 보관가능
 }
 
 
