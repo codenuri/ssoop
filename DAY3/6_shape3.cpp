138b954b237ffeb62e42a2ff4aa50f952f81bd02
ecourse <smkang@codenuri.co.kr>
2024-09-04 15:47:40 +0900

    Add files via upload

---
+++ DAY3/6_shape3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+class Shape
+{
+	int color;
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1)
+		{
+			v.push_back(new Rect );
+		}
+		else if (cmd == 2)
+		{
+			v.push_back(new Circle); 
+		}
+		else if (cmd == 9)
+		{
+			// 모든 도형을 화면에 그리기
+			for (auto s : v)
+				s->draw();
+
+		}
+	}
+}
+
+// 어디에서 ?  왜 ? 위 코드가 에러일까요 ?
+// 해결책은 뭘까요 ??
+// => 다음소스에서 설명
+
+
+
