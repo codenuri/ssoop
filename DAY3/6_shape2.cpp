3f3670f0520d7ab8e9dc6d1b09d001d5a334cce0
ecourse <smkang@codenuri.co.kr>
2024-09-04 15:45:27 +0900

    Add files via upload

--- DAY3/6_shape2.cpp
+++ DAY3/6_shape2.cpp
@@ -29,11 +29,14 @@ int main()
 
 		if (cmd == 1)
 		{
+//			Rect rc; // 이렇게 만들면 {} 벗어날때 즉시 파괴 됩니다.
+			Rect* p = new Rect;
 
+			v.push_back(p);
 		}
 		else if (cmd == 2)
 		{
-
+			v.push_back(new Circle); // 위 코드는 그냥 이렇게 하면 됩니다.
 		}
 		else if (cmd == 9)
 		{
