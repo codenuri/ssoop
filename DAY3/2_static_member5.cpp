a96e1097730c95fb81283497843083bbc8e3a0ce
ecourse <smkang@codenuri.co.kr>
2024-09-04 10:02:57 +0900

    Add files via upload

--- DAY3/2_static_member5.cpp
+++ DAY3/2_static_member5.cpp
@@ -14,15 +14,18 @@ public:
 	Date(int year, int month, int day)
 		: year{ year }, month{ month }, day{ day }
 	{
+		// year = year; // 인자year = 인자year
+		// 하지만 위 초기화 리스트에서는 문제 없습니다.
 	}
 
 	// #2.setter/getter 제공. 
 	int get_year() const { return year; }
+	void set_year(int y) { if (y > 0 && y < 3000) year = y; }
 
-
+	// month, day 는 복습할때 해보세요
 };
 
 int main()
 {
-
+	Date today{ 2024, 9, 4 };
 }
