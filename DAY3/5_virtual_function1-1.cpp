834d80f3d4adf46325a27db3354cd5ff90d7f6f1
ecourse <smkang@codenuri.co.kr>
2024-09-04 15:26:25 +0900

    Add files via upload

--- DAY3/5_virtual_function1-1.cpp
+++ DAY3/5_virtual_function1-1.cpp
@@ -5,29 +5,34 @@
 class Animal
 {
 public:
+	// non-virtual : static binding 해달라는 의미.
+	//				 "포인터 타입에 따라 함수 결정"
 	void cry1() { std::cout << "Animal cry1" << std::endl; } // 1
 
-	void cry2() { std::cout << "Animal cry2" << std::endl; } // 1
+
+	// virtual : dynamic binding 해달라는 의미
+	//			 "실행시간에 포인터가 가리키는 객체 조사해서 함수 결정"
+	virtual void cry2() { std::cout << "Animal cry2" << std::endl; } // 1
 };
 
 class Dog : public Animal
 {
 public:
 	void cry1() { std::cout << "Dog cry1" << std::endl; }  // 2
-	void cry2() { std::cout << "Dog cry2" << std::endl; }  // 2
+
+	virtual void cry2() { std::cout << "Dog cry2" << std::endl; }  // 2
 };
+
 
 int main()
 {
 	Animal a;
 	Dog    d;
 
-	a.cry1();	// "Animal cry1"
-	d.cry1();   // "Dog cry1"
-
 	Animal* p = &d;
 
-	p->cry1(); 
+	p->cry1(); // static binding "Animal cry1"
+	p->cry2(); // dynamic binding  "Dog  cry2"
 }
 
 
