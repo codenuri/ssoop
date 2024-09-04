4e520d8de400b0e01f4f5f7b4a5ccdca15aa7d85
ecourse <smkang@codenuri.co.kr>
2024-09-04 13:14:06 +0900

    Add files via upload

--- DAY3/문자열2.c
+++ DAY3/문자열2.c
@@ -7,10 +7,8 @@ int main()
 
 	char* sp = "efgh";  
 
-	*sa = 'X';
-	*sp = 'X';
-
-
+	*sa = 'X'; // ok. stack 은 R/W 가능.
+//	*sp = 'X'; // runtime error. 읽기전용 메모리를 쓰려고 하기 때문에
 
 
 	printf("=====\n");
