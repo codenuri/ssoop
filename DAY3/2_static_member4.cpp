1d63c40cf4d17e4ccda07df505433997dde51d0c
ecourse <smkang@codenuri.co.kr>
2024-09-04 09:50:47 +0900

    Add files via upload

--- DAY3/2_static_member4.cpp
+++ DAY3/2_static_member4.cpp
@@ -4,23 +4,33 @@
 class Car
 {
 	int color = 0;
+	static int cnt; 
 public:
-	static int cnt; 	
-
 	Car() { ++cnt; }
 	~Car() { --cnt; }
+
+
+//	int get_count() const { return cnt; }	// 객체가 있어야만 호출가능
+											// "c1.get_count()"
+
+	static int get_count() { return cnt; }
+							// => static member 함수
+							// => 객체 없이 호출 가능
+							// => Car::get_count() 로 호출
+							// => 객체와 상관없이 호출가능하므로
+							//    "const member function" 으로 만들수는없다
+
 };
 int Car::cnt = 0; 
 
-
 int main()
 {
-	std::cout << Car::cnt << std::endl;
+	std::cout << Car::get_count() << std::endl;
 
 	Car c1;
 	Car c2;
 
-	std::cout << Car::cnt << std::endl;
+	std::cout << Car::get_count() << std::endl;
 
 }
 
