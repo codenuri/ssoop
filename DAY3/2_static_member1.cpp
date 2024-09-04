e412982db73f396e90feb0f44d7bd56a91cc5173
ecourse <smkang@codenuri.co.kr>
2024-09-04 09:05:55 +0900

    Add files via upload

--- DAY3/2_static_member1.cpp
+++ DAY3/2_static_member1.cpp
@@ -4,6 +4,9 @@
 
 // 객체가 몇개나 생성되었는지 갯수를 관리하고 싶다.
 // 방법 1. 멤버 데이타 사용
+// => 실패!!
+// => 멤버 데이타는 모든 객체가 따로 가지게 된다.
+// => 아래 코드에서 c1.cnt 와 c2.cnt 는 별도의 메모리 이다.
 
 class Car
 {
@@ -11,7 +14,7 @@ class Car
 public:
 	int cnt = 0;
 
-	Car() { ++cnt; }
+	Car()  { ++cnt; }
 	~Car() { --cnt; }
 };
 
@@ -20,6 +23,6 @@ int main()
 	Car c1;
 	Car c2;
 
-	std::cout << c1.cnt << std::endl;	
+	std::cout << c1.cnt << std::endl;	// 1
 	
 }
