4f2dd534892d8af6736595e77d1ca4035f13d861
ecourse <smkang@codenuri.co.kr>
2024-09-04 13:37:15 +0900

    Add files via upload

--- DAY3/3_inheritance1.cpp
+++ DAY3/3_inheritance1.cpp
@@ -11,6 +11,18 @@ class Person
 };
 
 // 기존 클래스(Person) 을 확장해서 새로운 타입을 설계 합니다.
+// => 상속(inheritance) 문법
+
+// C++     : class Student : public Person
+// C#      : class Student : Person
+// PYTHON  : class Student( Person )
+// Java    : class Student extends Person
+
+
+// Person  : Base Class(기반),  Super Class,   Parent Class
+// Student : Derived Class(파생), Sub Class,   Child Class 
+
+
 class Student : public Person
 {	
 	int    id;
