4855b673f71074209827668adedb7016e1c6aa6b
ecourse <smkang@codenuri.co.kr>
2024-09-04 15:20:48 +0900

    Add files via upload

--- DAY3/5_virtual_function1.cpp
+++ DAY3/5_virtual_function1.cpp
@@ -29,8 +29,32 @@ int main()
 	p->cry1(); // ???
 
 
-
 	// C++ / C# / Rust : Animal cry1
+
 	// java, python, swift, kotlin 등 대부분의 객체지향언어 : Dog cry1
 }
+
+// 아래 내용은 151p 있습니다.
+
+// "p->cry1()" 을 어느 함수와 연결할것인가 ?
+// => "function binding" 이라고 합니다.
+
+// static binding : 컴파일 시간에 컴파일러가 함수 호출을 결정
+//					컴파일러는 "p가 가리키는 곳의 객체의 타입"은 알수없다
+//					p 자체의 타입이 "Animal*" 라는 것만 알고 있다.
+//					"Animal cry1" 호출!
+// 논리적이지 않지만 빠르다.  early binding 이라고도 합니다.
+
+// C++/C# 언어 기본 바인딩
+
+// dynamic binding : 컴파일 시간에는 p 가 가리키는 메모리를 조사하는
+//					 기계어 코드 생성
+//					 실행시, p의 메모리를 조사해서 실제 객체에 타입에따라
+//					 함수 호출 결정
+//					 "p가 가리키는 곳에 Dog 객체가 있다면"
+//					 "Dog cry1" 호출
+// 논리적이지만 느리다.   late binding 이라고도 합니다
+// java, swift, python, kotlin 등 대부분의 객체지향 프로그래밍 언어. 
+// C++/C# 언어의 virtual function(가상함수)
+
 
