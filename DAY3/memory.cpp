93839fd045695ed76894941020ecb9ff52744594
ecourse <smkang@codenuri.co.kr>
2024-09-04 11:42:54 +0900

    Add files via upload

--- DAY3/memory.cpp
+++ DAY3/memory.cpp
@@ -1,4 +1,5 @@
 // memory.cpp
+#include <stdio.h>
 
 int g = 0x11223344;
 
@@ -6,10 +7,15 @@ int main()
 {
 	static int s = 0x55667788;
 
-	int n = 10;
+	int n = 10; // 스택에서 메모리를 할당하고
+				// 10으로 채우는 기계어 코드
 
-	printf("hello");
+	printf("hello\n");
 
-	const char* sp = "abcd";
-	char sa[] = "abcd";
+	printf("함수 주소 : %p\n", &main);
+	printf("전역변수 주소 : %p\n", &g);
+	printf("지역변수 주소 : %p\n", &n);
+
+//	const char* sp = "abcd";
+//	char sa[] = "abcd";
 }
