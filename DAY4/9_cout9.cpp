d2720f014db8959e16f9f9c55df0e3047e8b7bc4
ecourse <smkang@codenuri.co.kr>
2024-09-05 17:04:10 +0900

    Add files via upload

--- DAY4/9_cout9.cpp
+++ DAY4/9_cout9.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+
+// scanf, std::cin
+// => 모두 입력 버퍼에서 꺼내는 것입니다.
+// => 만약 입력버퍼가 비어 있으면
+// => "키보드 => 입력버퍼"에 담는 루틴 실행. enter 칠때 종료
 int main()
 {
 	int n = 0;
@@ -11,6 +16,11 @@ int main()
 		if (std::cin.fail() == true)
 		{
 			std::cout << "fail !!!\n";
+
+			// 다시입력 받기전에 "입력버퍼 비워야 합니다"
+			// C : fflush(stdin), C++ : 아래 코드
+			std::cin.ignore();
+
 			continue;
 		}
 		break;
