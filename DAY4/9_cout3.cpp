80be65a2189019facc1a15a0ed99f29f96c730e7
ecourse <smkang@codenuri.co.kr>
2024-09-05 16:00:24 +0900

    Add files via upload

--- DAY4/9_cout3.cpp
+++ DAY4/9_cout3.cpp
@@ -6,9 +6,11 @@ namespace std
 	class ostream
 	{
 	public:
-		void operator<<(int n) { printf("%d", n); }
-		void operator<<(double d) { printf("%f", d); }
-		void operator<<(const char* s) { printf("%s", s); }
+		// #1. << 연산자를 연속으로 하기 위해 자신을 참조로 반환
+		// #2. 아래 3개만 만들었지만, 실제 표준은 모든 기본타입에 대해서 제공
+		ostream& operator<<(int n) { printf("%d", n);         return *this;}
+		ostream& operator<<(double d) { printf("%f", d);      return *this;}
+		ostream& operator<<(const char* s) { printf("%s", s); return *this;}
 	};
 	ostream cout;
 }
@@ -19,6 +21,7 @@ int main()
 	double d = 3.4;
 
 	std::cout << n << ", " << d << "\n";
+	// cout.operator<<(n).operator<<(", ").operator<<(d).operator<<("\n")
 
 }
 
