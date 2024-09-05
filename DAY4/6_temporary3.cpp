c7b78e9ef4c4b97645e35b7090e2408bc62f3ed6
ecourse <smkang@codenuri.co.kr>
2024-09-05 14:18:59 +0900

    Add files via upload

--- DAY4/6_temporary3.cpp
+++ DAY4/6_temporary3.cpp
@@ -14,6 +14,7 @@ void draw_line(const Point& from, const Point& to) {}
 
 int main()
 {
+	/*
 	// (1,1) ~ (5,5) 에 선을 그리고 싶습니다.
 	Point p1(1,1);
 	Point p2(5,5);
@@ -21,11 +22,16 @@ int main()
 	draw_line(p1, p2); // 이렇게 하면 p1, p2 는
 						// 함수 호출뒤에도 계속 메모리에 있고
 						// {} 을 벗어나야 파괴 됩니다.
-
+	*/
 	// 임시객체로 전달
 	// => 인자 전달에만 사용되고,
 	// => 함수 호출이 종료 되면 파괴 됩니다.
-	//draw_line( Point(1, 1), Point(5, 5) );
+//	draw_line( Point(1, 1), Point(5, 5) );
+				// => 임시 객체 만들때 () 사용하면 함수호출 처럼 보입니다.
+	
+	draw_line(Point{ 1, 1 }, Point{ 5, 5 } );
+				// => () 대신 {} 초기화를 사용하면
+				//    확실히 함수 호출과는 구분 됩니다. 권장
 
 
 	std::cout << "-----" << std::endl;
