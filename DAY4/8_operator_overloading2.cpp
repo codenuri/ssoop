496386ce1e1069bc2ae9da4547f9d17cdec0f353
ecourse <smkang@codenuri.co.kr>
2024-09-05 15:23:51 +0900

    Add files via upload

---
+++ DAY4/8_operator_overloading2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+// 163 page ~
+class Point
+{
+	int x, y;
+public:
+	Point(int x, int y) : x(x), y(y) {}
+
+	void print() const { std::cout << x << ", " << y << std::endl; }
+};
+
+int main()
+{
+	Point p1(1, 1);
+	Point p2(2, 2);
+
+	Point p3 = p1 + p2; // operator+(p1, p2) 
+	
+
+	p3.print();
+}
+
