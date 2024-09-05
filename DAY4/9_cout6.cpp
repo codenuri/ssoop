34d10be72ccb1bb428405eb09d181df6d923c294
ecourse <smkang@codenuri.co.kr>
2024-09-05 16:39:23 +0900

    Add files via upload

---
+++ DAY4/9_cout6.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+namespace std
+{
+	class ostream
+	{
+	public:
+		ostream& operator<<(int n) { printf("%d", n);         return *this; }
+		ostream& operator<<(double d) { printf("%f", d);      return *this; }
+		ostream& operator<<(const char* s) { printf("%s", s); return *this; }
+	};
+	ostream cout;
+
+
+	std::ostream& endl(std::ostream& os)
+	{
+		os << "\n";
+		return os;
+	}
+}
+
+int main()
+{
+	std::cout << std::endl; // endl 의 정체는 함수 입니다.
+							// char* endl = "\n" 가 아닙니다.
+
+	std::endl( std::cout );   // 위 코드는 이코드와 동일합니다.
+}
+
+
