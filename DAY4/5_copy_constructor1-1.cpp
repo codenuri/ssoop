baf6123b03983cec06a860d8462c6e179e624893
ecourse <smkang@codenuri.co.kr>
2024-09-05 13:15:23 +0900

    Add files via upload

--- DAY4/5_copy_constructor1-1.cpp
+++ DAY4/5_copy_constructor1-1.cpp
@@ -10,13 +10,16 @@ public:
 	Point() : x{ 0 }, y{ 0 } {}	
 	Point(int a, int b) : x{ a }, y{ b } {}		
 
-	Point(const Point& other) : x(other.x), y(other.y) {}
+	Point(const Point& other) : x(other.y), y(other.x) {}
 };
 
 int main()
 {
-	Point p3(1, 2);	
+	Point p1(1, 2);	
 
-	Point p4(p3);   
-	Point p5 = p3; 
+	Point p2(p1);   
+	Point p3 = p1;  // 사용자가 만든 복사 생성자 호출
+					// 사용자 구현은 x, y 를 바꾸어서 복사
+	std::cout << p3.x << std::endl; // 2
+	std::cout << p3.y << std::endl; // 1
 }
