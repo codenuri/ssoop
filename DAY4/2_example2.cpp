d802cc5742a60076f65725650b279aa09a046b1f
ecourse <smkang@codenuri.co.kr>
2024-09-05 09:49:23 +0900

    Add files via upload

---
+++ DAY4/2_example2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h>
+
+class MenuItem
+{
+	std::string title;
+	int id;
+public:
+	MenuItem(const std::string& title, int id)
+		: title{ title }, id{ id } {}
+
+	std::string get_title() const { return title; }
+
+	void command()
+	{
+		std::cout << get_title() << " 메뉴 선택\n";
+
+		_getch(); 
+	}
+
+};
+
+int main()
+{
+	MenuItem m1("김밥", 11);
+	MenuItem m2("라면", 12);
+
+	PopupMenu pm("오늘의 점심");
+
+	pm.add(&m1);
+	pm.add(&m2);
+
+	pm.command(); // 팝업 메뉴 선택시 동작을 생각해 보세요
+}
