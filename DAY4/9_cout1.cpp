da3600f1bbb4723001f36c8d934e63411586d717
ecourse <smkang@codenuri.co.kr>
2024-09-05 15:49:35 +0900

    Add files via upload

---
+++ DAY4/9_cout1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// cout ÀÇ ¿ø¸® ( 169 ~ )
+int main()
+{
+	int    n = 10;
+	double d = 3.4;
+
+	std::cout << n; // cout.operator<<(int)
+	std::cout << d; // cout.operator<<(double)
+	
+}
+
+
