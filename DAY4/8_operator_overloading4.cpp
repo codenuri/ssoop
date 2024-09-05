56be8c3a73516498d056de526e940e8c3d7ff584
ecourse <smkang@codenuri.co.kr>
2024-09-05 15:39:15 +0900

    Add files via upload

--- DAY4/8_operator_overloading4.cpp
+++ DAY4/8_operator_overloading4.cpp
@@ -32,9 +32,21 @@ int main()
 	Point p1(1, 1);
 	Point p2(2, 2);
 
-	Point p3 = p1 + p2; 
+	Point p3 = p1 + p2; // error.
+						// 일반함수(non-member function) 와 
+						// 멤버함수 중 한개만 만드세요
 
+	// #1. 멤버와 일반함수중 어떤것이 좋을까요 ?
+	// => 다양한 의견이 있습니다.
+	// => 그런데
+	// 객체의 상태가 변하는 연산자 : 멤버가 좋다.( a += b)
+	// 객체의 상태가 변하지 않은 연산자 : 일반함수가 좋다는 의견이 우세
+	//									(a + b, a - b)
 
-	p3.print();
+	int n = 3;
+	Point p4 = p1 + p2;
+	Point p5 = p1 + n;
+	Point p6 = n  + p1;
+
 }
 
