4e672820f8445e50e125b534933e1911a4386555
ecourse <smkang@codenuri.co.kr>
2024-09-05 14:37:23 +0900

    Add files via upload

--- DAY4/6_temporary5.cpp
+++ DAY4/6_temporary5.cpp
@@ -10,13 +10,21 @@ public:
 		return *this;
 	}
 	int get() const { return cnt; }
+
+	Counter(const Counter& other) : cnt(other.cnt)
+	{
+		std::cout << "Counter(const Counter& other)\n";
+	}
+	~Counter() { std::cout << "~Counter()\n"; }
 };
 
 int main()
 {
 	Counter c;
 
+	std::cout << "==================\n";
 	c.increment().increment().increment();
+	std::cout << "==================\n";
 
 	std::cout << c.get() << std::endl;
 }
