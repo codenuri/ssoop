314a5b701bba3a81f351ba17261109c1e7120be9
ecourse <smkang@codenuri.co.kr>
2024-09-05 15:34:41 +0900

    Add files via upload

---
+++ DAY4/8_operator_overloading4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+// 163 page ~
+class Point
+{
+	int x, y;
+public:
+	Point(int x, int y) : x(x), y(y) {}
+
+	void print() const { std::cout << x << ", " << y << std::endl; }
+
+
+	Point operator+(const Point& pt) const
+	{
+		Point ret(x + pt.x, y + pt.y);
+		return ret;
+	}
+
+
+	friend Point operator+(const Point& p1, const Point& p2);
+
+};
+
+Point operator+(const Point& p1, const Point& p2)
+{
+	Point ret(p1.x + p2.x, p1.y + p2.y);
+
+	return ret;
+}
+
+int main()
+{
+	Point p1(1, 1);
+	Point p2(2, 2);
+
+	Point p3 = p1 + p2; 
+
+
+	p3.print();
+}
+
