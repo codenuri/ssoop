8f2279e38b74a928723e5f3ca7718f14ef0c6528
ecourse <smkang@codenuri.co.kr>
2024-09-05 16:35:08 +0900

    Add files via upload

--- DAY4/9_cout4.cpp
+++ DAY4/9_cout4.cpp
@@ -25,8 +25,9 @@ int main()
 {
 	Point p(1, 2);
 
+	// operator<<(cout, p)
 	std::cout << p ; // cout.operator<<(Point) 가 있어야 합니다.
-					 // => 당연히 operator<<(Point) 버전은 없습니다.
+//					 // => 당연히 operator<<(Point) 버전은 없습니다.
 					 // => C++ 표준 ostream 에 멤버 추가도 안됩니다.
 					 // 
 					 // 하지만, 일반 함수로는 가능합니다.
