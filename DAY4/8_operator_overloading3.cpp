9ad73a5e22dcb1d1a00a86488ee4011dd2649dc4
ecourse <smkang@codenuri.co.kr>
2024-09-05 15:33:14 +0900

    Add files via upload

--- DAY4/8_operator_overloading3.cpp
+++ DAY4/8_operator_overloading3.cpp
@@ -10,16 +10,17 @@ public:
 
 	// 멤버 함수로 구현한 operator+
 	// +은 이항연산자 이지만 멤버로 구현할때는 인자가 한개!!
-	Point operator+(const Point& pt)
+	// 주의 ! : "p1+p2" 에서 p1이 상수 객체라도 호출가능해야 합니다.
+	//		    상수멤버 함수로 해야 합니다.
+	Point operator+(const Point& pt) const 
 	{
 		Point ret(x + pt.x, y + pt.y);
 		return ret;
 	}
 };
-
 int main()
 {
-	Point p1(1, 1);
+	const Point p1(1, 1);
 	Point p2(2, 2);
 
 	Point p3 = p1 + p2; // "operator+(p1, p2)" 를 찾거나
