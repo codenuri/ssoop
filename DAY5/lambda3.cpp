733fd4069ed9c2835aef00a0ceb5df7ba4781eb8
ecourse <smkang@codenuri.co.kr>
2024-09-06 14:31:38 +0900

    Add files via upload

--- DAY5/lambda3.cpp
+++ DAY5/lambda3.cpp
@@ -9,6 +9,7 @@ bool foo(int a) { return a % 3 == 0; }
 struct IsDivide
 {
 	int value;
+
 	IsDivide(int n) : value(n) {}
 
 	bool operator()(int n) { return n % value == 0; }
@@ -23,10 +24,13 @@ int main()
 
 	int k = 3; 
 
-	IsDivide f(k);
+	IsDivide f(k); // f는 내부적으로 3을 보관
 
-	bool b = f(10);
+	bool b = f(10); // f는 단항함수 입니다.
+					// bool b = 10 % k  의 의미 입니다.
+					
+					// => 결국 f는 "k값을 캡쳐한 단항함수" 입니다
 
-	auto ret1 = std::find_if(v.begin(), v.end(), ? );
+	auto ret1 = std::find_if(v.begin(), v.end(), f );
 
 }
