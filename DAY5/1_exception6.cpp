eb7e5f19223607dc70238b05c45ff3f8e83892c4
ecourse <smkang@codenuri.co.kr>
2024-09-06 09:05:34 +0900

    Add files via upload

--- DAY5/1_exception6.cpp
+++ DAY5/1_exception6.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+// 예외가 발생해서 catch 로 이동할때
+// => 중간에 만들어진 모든 지역변수는 안전하게 파괴 됩니다.
+// => stack unwinding ( 스택 풀기 )라고합니다.
+
+
 class Point
 {
 public:
@@ -10,11 +15,13 @@ void goo()
 {
 	Point p2;
 	throw 1; 
+	std::cout << "goo\n";
 }
 void foo()
 {
 	Point p2;
 	goo();
+	std::cout << "foo\n";
 }
 int main()
 {
