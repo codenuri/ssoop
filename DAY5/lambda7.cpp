29c4b6dcedbfe19e2d7ba6e9c574f813283ed7ee
ecourse <smkang@codenuri.co.kr>
2024-09-06 15:14:22 +0900

    Add files via upload

--- DAY5/lambda7.cpp
+++ DAY5/lambda7.cpp
@@ -6,6 +6,19 @@ int main()
 {
 	int k = 0;
 
-	auto f1 = [k] () { k = 10; };
-	auto f3 = [&k]() { k = 10; };
+	auto f1 = [k] ()         { k = 10; }; // error. capture by value
+	auto f2 = [k] () mutable { k = 10; }; // ok. 그런데, 복사본에 쓴것
+	auto f3 = [&k]()         { k = 10; }; // ok  capture by reference
+
+	// f1 원리
+	class GeneratedName1
+	{
+		int k;
+	public:
+		GeneratedName1(int n) : k(n) {}
+		void operator()() const
+		{
+			k = 10;
+		}
+	};
 }
