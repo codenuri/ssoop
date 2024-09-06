11e7f6ff52b6290be2d4cc851cb020557980cb26
ecourse <smkang@codenuri.co.kr>
2024-09-06 09:14:53 +0900

    Add files via upload

--- DAY5/1_exception7.cpp
+++ DAY5/1_exception7.cpp
@@ -8,12 +8,22 @@ int main()
 {
 	std::vector<int> v(3);
 
+	// at : if 문으로 index 조사후 잘못된 경우 예외 발생
+	// [] : 전달된 index 를 그대로 사용 - 빠르게 동작!!
+	//	   => index 가 안전하다면 at 보다 [] 가 좋다
+
 	try
 	{
-		v.at(10) = 0;
+//		v.at(10) = 0; // 잘못된 인덱스 사용시 예외 발생
+		v[10] = 0;	  // 잘못된 인덱스 사용시 잘못된 메모리참조!
 	}
 	catch (std::out_of_range& e)
 	{
 		std::cout << "잘못된 index 사용\n";
+	}
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		v[i] = 0;
 	}
 }
