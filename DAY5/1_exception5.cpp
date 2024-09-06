5cc7200f60b1f438ee1952e2c208e4d097c713c6
ecourse <smkang@codenuri.co.kr>
2024-09-06 09:00:33 +0900

    Add files via upload

--- DAY5/1_exception5.cpp
+++ DAY5/1_exception5.cpp
@@ -3,6 +3,13 @@
 void goo()
 {
 	// 여기서 실패 했다
+	// 반환값으로 알려주는 경우 : foo 을 거쳐서 main 에 알려야 합니다.
+	// 예외는 한번에 전달도 가능합니다.
+	throw 1; // 이순간 main 의 catch 로 바로 이동
+			 // => 장점으로 보는 시각도 있고
+			 // => 단점으로 보는 시작도 있습니다.
+			 // 구글 C++ style guide : 예외 사용하지 말라
+			 // MS  C++ style guide : 예외 사용해라
 }
 void foo()
 {
@@ -10,5 +17,9 @@ void foo()
 }
 int main()
 {
-	foo();
+	try
+	{
+		foo();
+	}
+	catch (int e) {}
 }
