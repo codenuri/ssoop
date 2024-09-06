7b1c524212b84bb9368168b7dd571f2595761411
ecourse <smkang@codenuri.co.kr>
2024-09-06 14:37:33 +0900

    Add files via upload

--- DAY5/lambda3.cpp
+++ DAY5/lambda3.cpp
@@ -3,7 +3,13 @@
 #include <list>
 #include <algorithm>
 
+// 함수 : 동작은 있지만 상태가 없습니다.
+
+// 함수객체 : 동작도 있고, 상태도 있습니다.(멤버 데이타가 있다는 의미)
+
 bool foo(int a) { return a % 3 == 0; }
+
+
 
 // 아래 코드 잘생각해보세요
 struct IsDivide
@@ -33,4 +39,15 @@ int main()
 
 	auto ret1 = std::find_if(v.begin(), v.end(), f );
 
+
+	// 한번만 사용한다면 임시객체로
+	auto ret1 = std::find_if(v.begin(), v.end(), IsDivide{ k } );
+						// => "IsDivide{k}" 는 함수 인자로
+						// => "단항함수 + 지역변수 k의 값" 을 같이전달 
 }
+
+// closure : 자신이 속한 문맥의 지역변수를 캡쳐할수 있는
+//			 "인자로 전달 가능한" 함수(객체)
+
+// C++ 일반함수 : Closure 가 될수 없습니다.
+//     함수객체 : Closure 가 될수 있습니다.
