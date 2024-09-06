17b2386db9b6eff281ea51e773e2cee61998ccdc
ecourse <smkang@codenuri.co.kr>
2024-09-06 11:06:44 +0900

    Add files via upload

--- DAY5/4_iterator3-1.cpp
+++ DAY5/4_iterator3-1.cpp
@@ -11,6 +11,17 @@ int main()
 		std::cout << e << ", ";
 	}
 
-	// 위 코드의 원리를 알아 봅시다.
+	// 위 코드의 원리를 알아 봅시다.(C#, java, python 모두 동일)
+	// 위 코드를 컴파일러는 아래 처럼 변경
+
+	auto first = c.begin();
+	auto last = c.end();
+
+	for (; first != last; ++first)
+	{
+		auto e = *first;
+		//------------------------------
+		std::cout << e << ", ";
+	}
 }
 
