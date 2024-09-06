0be05533c924602a4407f0a48b2f36a62849e383
ecourse <smkang@codenuri.co.kr>
2024-09-06 14:21:14 +0900

    Add files via upload

--- DAY5/lambda1.cpp
+++ DAY5/lambda1.cpp
@@ -11,6 +11,15 @@ struct Plus
 
 int main()
 {
-	Plus p;
-	int n = p(1, 2); // ??
+	Plus p; // p 는 함수가 아닌 객체 입니다. Plus 타입의 객체
+
+	int n1 = p(1, 2); // 객체 p를 함수 처럼 사용합니다. 함수 객체라고합니다.
+	int n2 = p.operator()(1, 2); // 이렇게 직접 호출해도 됩니다.
+
+	// p1 + p2 => p1.operator+(p2)
+
+
 }
+
+
+
