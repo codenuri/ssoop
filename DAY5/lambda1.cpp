81799c6ae56ccf197510e6f90b6639acdbf0bcab
ecourse <smkang@codenuri.co.kr>
2024-09-06 14:23:16 +0900

    Add files via upload

--- DAY5/lambda1.cpp
+++ DAY5/lambda1.cpp
@@ -1,6 +1,16 @@
 // lambda1.cpp
 #include <iostream>
 
+// function object
+// => () 연산자를 재정의 해서 함수 처럼 사용가능한 객체
+
+// 왜 ?? 일반 함수를 만들면 되는데 ??
+
+// 1. 상태를 가지는 함수
+// 2. Closure
+// 3. 그외에도 장점이 너무 많습니다.
+// 4. C++20 이후는 모든 표준 함수는 함수가 아닙니다.(함수객체 입니다.)
+
 struct Plus
 {
 	int operator()(int a, int b)
